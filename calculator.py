53bbf2ffba69a6c6ec285051b9bf575b2a0073d0
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 16:07:56 -0700

    Square through mod functions.

--- calculator.py
+++ calculator.py
@@ -26,4 +26,15 @@ while True:
     elif tokens[0] == "/":
         result = divide(tokens[1], tokens[2])
         print result
-
+    elif tokens[0] == "square":
+        result = square(int(tokens[1]))
+        print result
+    elif tokens[0] == "cube":
+        result = cube(int(tokens[1]))
+        print result
+    elif tokens[0] == "pow":
+        result = power(int(tokens[1]), int(tokens[2]))
+        print result
+    elif tokens[0] == "mod":
+        result = mod(int(tokens[1]), int(tokens[2]))
+        print result
