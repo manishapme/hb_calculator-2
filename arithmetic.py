d27b72b6d3a7ea1ef9c2743d232c4998ebbf5383
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 18:02:42 -0700

    optimized to take more numbers

--- arithmetic.py
+++ arithmetic.py
@@ -6,39 +6,45 @@ def add(num_list):
     return results
 
 def subtract(num_list):
-    """Return the num1 minus num2."""
+    """Return the difference from a list of numbers."""
 
     results = float(num_list[0])
     for i in range(1,len(num_list)):
         results = results - float(num_list[i])
     return results
 
-def multiply(num1, num2):
-    """Return num1 multiplied by num2."""
+def multiply(num_list):
+    """Return total from multiplying a series of numbers."""
 
-    return num1 * num2
+    results = float(num_list[0])
+    for i in range(1,len(num_list)):
+        results = results * float(num_list[i])
+    return results
 
-def divide(num1, num2):
+def divide(num_list):
     """Return num1 divided by num2."""
+    results = float(num_list[0])
+    for i in range(1,len(num_list)):
+        results = results / float(num_list[i])
+    return results
 
-    return float(num1) / float(num2)
 
-def square(num1):
+def square(num_list):
     """Return square root."""
 
-    return num1**2
+    return float(num_list[0])**2
 
-def cube(num1):
+def cube(num_list):
     """Return cube root."""
 
-    return num1**3
+    return float(num_list[0])**3
 
-def power(num1, num2):
+def power(num_list):
     """Return num1 raised to the power of num2."""
 
-    return num1**num2
+    return float(num_list[0])**float(num_list[1])
 
-def mod(num1, num2):
+def mod(num_list):
     """Return the remainder of num1 divided by num2."""
 
-    return num1 % num2
+    return float(num_list[0]) % float(num_list[1])
